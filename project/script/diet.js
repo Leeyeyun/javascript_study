5d4e667726a7cbd7689ed182349c4a4a498c7f7c
Leeyeyun <165612381+Leeyeyun@users.noreply.github.com>
2024-05-21 18:16:04 +0900

    js

--- project/script/diet.js
+++ project/script/diet.js
@@ -4,3 +4,15 @@
 - 버튼을 눌렀을 때 최종 목표 칸에 메세지 출력하기
 결과예시) 적정체중은 ?kg이며 ?kg 초과되셨습니다.
 */
+const height = document.querySelector("#height")
+const weight = document.querySelector("#weight")
+const goals = document.querySelector("#goals")
+const btn = document.querySelector("button")
+console.log(height, weight, btn)
+btn.addEventListener('click',goal_func)
+function goal_func(){
+    let user_h = Number(height.value)
+    let user_w = Number(weight.value)
+    let calc = (user_h - 100) * 0.9
+    goals.value = `적정체중은 ${calc}kg이며, ${user_w - calc}kg 초과되셨습니다. 노력하세요.`
+}
