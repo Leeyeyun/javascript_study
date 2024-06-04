03e8f1d0cd6261aff9b5a03697be57b2b0f66e7e
Leeyeyun <165612381+Leeyeyun@users.noreply.github.com>
2024-06-04 17:56:51 +0900

    캐스퍼 홈페이지 실습

--- project/casper_new/script/script.js
+++ project/casper_new/script/script.js
@@ -3,8 +3,9 @@ const sub = document.querySelectorAll('.sub')
 const sub_bg = document.querySelector('.sub_bg')
 const nav = document.querySelector('nav')
 const nav_clone = nav.cloneNode(true) //nav복제 변수
-const m_nav = document.querySelector('.m_nav')
-console.log(sub, sub_bg, nav, nav_clone, m_nav) //복제 대상 붙여넣기 용
+const m_nav = document.querySelector('.m_nav') //복제 대상 붙여넣기 용
+const all_nav = document.querySelector('.all_nav')
+console.log(sub, sub_bg, nav, nav_clone, m_nav, all_nav)
 
 //모든 서브, 서브배경 숨기기
 for(let i of sub)(i.style.display = 'none')
